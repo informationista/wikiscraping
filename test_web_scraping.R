aa4d55814fc80b7afb1cae684a20f9bd07afd725
lmfederer <lisa.federer@nih.gov>
2016-12-20 09:15:17 -0500

    Completes the wikipedia scraping portion of the code.  Replaces the get_day function with get_data, that gets both the list of deaths and the day at the same time.  Also includes some processing to clean up the data.

--- test_web_scraping.R
+++ test_web_scraping.R
@@ -1,11 +1,12 @@
 library(rvest)
 library(RCurl)
 library(plyr)
-
+library(tidry)
+library(ggplot2)
 
 ##create a list of URLs to retrieve
 ##create the list of years of interest
-year_list <- as.list(c(2013:2016))
+year_list <- as.list(c(2004:2016))
 
 url_df <- function(year){
   url <- paste("https://en.wikipedia.org/wiki/Deaths_in_", month.name, "_", year, sep = "")
@@ -13,25 +14,59 @@ url_df <- function(year){
 }
 df <- lapply(year_list, url_df) %>% rbind.fill()
 
+##this part got the day only - this got combined with the next part where we get the day and the deaths at the same time
+#get_day <- function(year, month, url){
+#  day <- read_html(as.character(url)) %>% html_nodes(xpath =  '//h3') %>% html_text()
+#  day <- gsub("\\[edit\\]", "", day) %>% as.numeric(as.character(day)) #remove the edit text and non-numerics
+#  day <- day[!is.na(day)] #get rid of NAs introduced in last part
+#  data.frame(year, month, day)
+#}
 
 
-##get the data
-#for (i in 1:4) ##length(all_year_urls)
- #days <- read_html(urls[i]) %>% html_nodes(xpath = '//h3') %>% html_text()
-#Sys.sleep(5)
-
-get_day <- function(dataset, year, month, url){
-  day <- read_html(as.character(dataset[url])) %>% html_nodes(xpath =  '//h3') %>% html_text()
-  day <- gsub("\\[edit\\]", "", day) %>% as.numeric(as.character(day)) #remove the edit text and non-numerics
-  day <- day[!is.na(day)] #get rid of NAs introduced in last part
-  data.frame(dataset[year], dataset[month], day)
+##this function takes the data frame and retrieves the days and deaths on those days for each url
+get_data <- function(year, month, url){
+    day <- read_html(as.character(url)) %>% html_nodes(xpath =  '//h3') %>% html_text()
+    day <- gsub("\\[edit\\]", "", day) %>% as.numeric(as.character(day)) #remove the edit text and non-numerics
+    day <- day[!is.na(day)] #get rid of NAs introduced in last part
+   df_mid <- data.frame(year, month, day, "death")
+   df_mid$death <- as.character(df_mid$X.death.)
+   df_mid <- df_mid[, -4] ##fix the data frame so it can take the character input from death
+  death <- read_html(as.character(url)) %>%  html_nodes("div#mw-content-text.mw-content-ltr") %>% html_nodes("ul") %>% html_text()
+  death <- death[c(-1, -2)] #remove the extraneous content
+  for (i in 1:nrow(df_mid)){
+    df_mid[i, 4] <- as.character(death[i])
+  }
+  return(df_mid)
 }
 
-test <- get_day(df[1, ], "year", "month.name", "url")
-
-
-test <- data.frame
-for (i in 1:2){
-  test1 <- get_day(df[i, 1], df[i, 2], df[i, 3])
-  test <- rbind(test, test1)
+##use the get day function to retrieve all data for all years/months in the list
+df_data <- data.frame()
+for (i in 1:nrow(df)){
+  test1 <- get_data(df[i, 1], df[i, 2], df[i, 3])
+  df_data <- rbind(df_data, test1)
 }
+
+##Now to clean up the death data
+#remove citation links at end of line
+df_data$death <- gsub("\\[[0-9]*\\]", "", df_data$death)
+
+##split each person into their own line
+df_data <- df_data %>% mutate(death = strsplit(as.character(death), "\n")) %>% unnest(death)
+
+##remove any blank lines
+df_data <- subset(df_data, death != "")
+
+##split the name and age into separate columns, dump the other info
+cleaned_df <- separate(df_data, col = death, into = c("name", "age"), sep = ",", remove = TRUE, extra = "drop")
+
+##remove any stuff that got into the age column that's not an actual age
+cleaned_df$age <- as.numeric(as.character(cleaned_df$age))
+
+##put months in ordered factor for nice charts
+cleaned_df$month <- factor(cleaned_df$month, levels = month.name)
+
+
+##Let's make some charts!
+qplot(year, data = cleaned_df, geom = "bar")
+
+
